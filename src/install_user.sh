5a1f624e0f2a8db990a0fd26543b37d636aec137
Emmanuel <emmanuelm41@gmail.com>
2021-02-11 16:55:04 -0300

    Add rust 1.47 support

--- src/install_user.sh
+++ src/install_user.sh
@@ -5,4 +5,5 @@ set -e
 go get -u -v github.com/tcnksm/ghr
 
 curl https://sh.rustup.rs -sSf | bash -s -- -y
+/tmp/install_rust.sh 1.47.0         # Install rust 1.47 (required by zcash apps)
 /tmp/install_rust.sh 1.49.0         # Install rust 1.49 (required by substrate apps)
