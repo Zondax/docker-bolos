e5cfaf521e185e8fd5dfd0c6ccb6129b1af7098e
Emmanuel <emmanuelm41@gmail.com>
2021-07-08 09:21:13 -0300

    add rust 1.53 support

--- src/install_user.sh
+++ src/install_user.sh
@@ -8,3 +8,4 @@ curl https://sh.rustup.rs -sSf | bash -s -- -y
 #/tmp/install_rust.sh 1.47.0         # Install rust 1.47 (required by zcash apps)
 #/tmp/install_rust.sh 1.49.0         # Install rust 1.49 (required by substrate apps)
 /tmp/install_rust.sh 1.51.0         # Install rust 1.51 (required by substrate apps)
+/tmp/install_rust.sh 1.53.0         # Install rust 1.53 (required by tezos apps)
