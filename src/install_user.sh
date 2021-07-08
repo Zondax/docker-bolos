0b8fb6e93f92bc76d1c75917fa32957d688e8a85
Emmanuel <emmanuelm41@gmail.com>
2021-07-08 09:56:19 -0300

    remove previous rust version

--- src/install_user.sh
+++ src/install_user.sh
@@ -7,5 +7,5 @@ go get -u -v github.com/tcnksm/ghr
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 #/tmp/install_rust.sh 1.47.0         # Install rust 1.47 (required by zcash apps)
 #/tmp/install_rust.sh 1.49.0         # Install rust 1.49 (required by substrate apps)
-/tmp/install_rust.sh 1.51.0         # Install rust 1.51 (required by substrate apps)
-/tmp/install_rust.sh 1.53.0         # Install rust 1.53 (required by tezos apps)
+#/tmp/install_rust.sh 1.51.0         # Install rust 1.51 (required by substrate apps)
+/tmp/install_rust.sh 1.53.0         # Install rust 1.53 (required by substrate apps)
