b574ddb4be98ef4c619f031364507a83fd6c7b83
becominginsane <francesco@zondax.ch>
2022-02-16 18:49:08 +0100

    fix: remove clippy and rustfmt explicit installs

--- src/install_rust.sh
+++ src/install_rust.sh
@@ -3,6 +3,4 @@ set -e
 . $HOME/.cargo/env && rustup toolchain install $@
 . $HOME/.cargo/env && rustup target add thumbv6m-none-eabi --toolchain $@
 . $HOME/.cargo/env && rustup target add thumbv8m.main-none-eabi --toolchain $@
-. $HOME/.cargo/env && rustup component add rustfmt --toolchain $@
-. $HOME/.cargo/env && rustup component add clippy --toolchain $@
 . $HOME/.cargo/env && rustup default $@
