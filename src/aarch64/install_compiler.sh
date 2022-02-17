3a4972311418ea86b6053bc1bee9a39da876c1be
becominginsane <francesco@zondax.ch>
2022-02-17 14:09:32 +0100

    fix(aarch64): install_compiler.sh contents

--- src/aarch64/install_compiler.sh
+++ src/aarch64/install_compiler.sh
@@ -7,21 +7,21 @@ rm -rf ${DEST} || true
 mkdir -p ${DEST}
 cd ${DEST}
 
-wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2
-wget https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+wget https://developer.arm.com/-/media/Files/downloads/gnu-rm/10-2020q4/gcc-arm-none-eabi-10-2020-q4-major-aarch64-linux.tar.bz2
+wget https://github.com/llvm/llvm-project/releases/download/llvmorg-12.0.0/clang+llvm-12.0.0-aarch64-linux-gnu.tar.xz
 
-sha256sum clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
-sha256sum gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2
+sha256sum clang+llvm-12.0.0-aarch64-linux-gnu.tar.xz
+sha256sum gcc-arm-none-eabi-10-2020-q4-major-aarch64-linux.tar.bz2
 
-echo "a9ff205eb0b73ca7c86afc6432eed1c2d49133bd0d49e47b15be59bbf0dd292e  clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz" | sha256sum --check
-echo "21134caa478bbf5352e239fbc6e2da3038f8d2207e089efc96c3b55f1edcd618  gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2" | sha256sum --check
+echo "d05f0b04fb248ce1e7a61fcd2087e6be8bc4b06b2cc348792f383abf414dec48  clang+llvm-12.0.0-aarch64-linux-gnu.tar.xz" | sha256sum --check
+echo "343d8c812934fe5a904c73583a91edd812b1ac20636eb52de04135bb0f5cf36a  gcc-arm-none-eabi-10-2020-q4-major-aarch64-linux.tar.bz2" | sha256sum --check
 
 echo "decompress gcc..."
-tar xfj gcc-arm-none-eabi-10-2020-q4-major-x86_64-linux.tar.bz2
+tar xfj gcc-arm-none-eabi-10-2020-q4-major-aarch64-linux.tar.bz2
 echo "decompress clang..."
-tar xf clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04.tar.xz
+tar xf clang+llvm-12.0.0-aarch64-linux-gnu.tar.xz
 
-mv clang+llvm-12.0.0-x86_64-linux-gnu-ubuntu-20.04 clang-arm-fropi
+mv clang+llvm-12.0.0-aarch64-linux-gnu clang-arm-fropi
 chmod 757 -R clang-arm-fropi/
 chmod +x clang-arm-fropi/bin/clang
 
