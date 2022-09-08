27f9fc7e6f0a5600710f8eba610a5a44a396691e
Juan Leni <juan.leni@zondax.ch>
2022-09-09 01:08:42 +0200

    remove ghr

--- src/install_user.sh
+++ src/install_user.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -e
 
-# Install ghr
-go get -u -v github.com/tcnksm/ghr
-
 curl https://sh.rustup.rs -sSf | bash -s -- -y
 #/tmp/install_rust.sh 1.47.0         # Install rust 1.47 (required by zcash apps)
 #/tmp/install_rust.sh 1.49.0         # Install rust 1.49 (required by substrate apps)
