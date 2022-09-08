21c7ab8605c9945e02a8dd222af081a546063f96
Juan Leni <juan.leni@zondax.ch>
2022-09-09 01:11:31 +0200

    upgrade rust

--- src/install_user.sh
+++ src/install_user.sh
@@ -6,4 +6,4 @@ curl https://sh.rustup.rs -sSf | bash -s -- -y
 #/tmp/install_rust.sh 1.49.0         # Install rust 1.49 (required by substrate apps)
 #/tmp/install_rust.sh 1.51.0         # Install rust 1.51 (required by substrate apps)
 #/tmp/install_rust.sh 1.53.0         # Install rust 1.53 (required by substrate apps)
-/tmp/install_rust.sh 1.58.1         # Install rust 1.58.1 (required by substrate apps)
+/tmp/install_rust.sh 1.61.0         # Install rust 1.61.0 (required by substrate apps)
