ceda980c24a7036f98ddc6d93a9d9c00bc43af23
Juan Leni <juan.leni@zondax.ch>
2022-09-10 10:59:15 +0200

    move to zondax_circle

--- src/create_users.sh
+++ src/create_users.sh
@@ -2,8 +2,8 @@
 set -e
 
 adduser --disabled-password --gecos "" -u 1000 zondax
-adduser --disabled-password --gecos "" -u 1001 zondax_circleci
+adduser --disabled-password --gecos "" -u 1001 zondax_circle
 adduser --disabled-password --gecos "" -u 501 zondax_mac
 echo "zondax_mac ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 echo "zondax ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-echo "zondax_circleci ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "zondax_circle ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
